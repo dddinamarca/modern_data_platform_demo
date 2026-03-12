f22cc98a546e6cdb2b87f27da5e82f06368a7f4d
dddinamarca <45823351+dddinamarca@users.noreply.github.com>
2026-03-12 17:42:08 -0300

    Update bronze_to_silver.py

--- batch-pipelines/bronze_to_silver.py
+++ batch-pipelines/bronze_to_silver.py
@@ -2,5 +2,4 @@ df = spark.read.format("delta").load("lakehouse/bronze/sales")
 
 clean = df.filter("price > 0")
 
-clean.write.format("delta").mode("overwrite") \
-    .save("lakehouse/silver/sales_clean")
+clean.write.format("delta").mode("overwrite").save("lakehouse/silver/sales_clean")
