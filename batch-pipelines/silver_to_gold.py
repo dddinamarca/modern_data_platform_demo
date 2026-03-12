26fd57f1620fedd8f7a9e2442d3025083383bdc3
dddinamarca <45823351+dddinamarca@users.noreply.github.com>
2026-03-12 17:42:50 -0300

    Update silver_to_gold.py

--- batch-pipelines/silver_to_gold.py
+++ batch-pipelines/silver_to_gold.py
@@ -1,10 +1,5 @@
 df = spark.read.format("delta").load("lakehouse/silver/sales_clean")
 
-metrics = df.groupBy("product") \
-    .agg(
-        sum("quantity").alias("units_sold"),
-        sum(expr("price * quantity")).alias("revenue")
-    )
+metrics = df.groupBy("product").agg(sum("quantity").alias("units_sold"),sum(expr("price * quantity")).alias("revenue"))
 
-metrics.write.format("delta").mode("overwrite") \
-    .save("lakehouse/gold/product_metrics")
+metrics.write.format("delta").mode("overwrite").save("lakehouse/gold/product_metrics")
